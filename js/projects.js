d2980eb98e92f9cdc85b52dbb30ecbc804d66958
Elluzion <dyrex2004@gmail.com>
2021-02-10 21:43:46 +0100

    add slight animations

--- js/projects.js
+++ js/projects.js
@@ -2,7 +2,7 @@ function onLoad() {
     $.getJSON('https://raw.githubusercontent.com/elluzion/infra/main/website/projects.json', { get_param: 'value' }, function(data) {
         $.each(data, function(index, element) {
             var projectID = element.id;
-            var html = '<div class="projects-single" id="' + element.id + "-" + element.type + '" onclick="loadDLPage(this.id)">' +
+            var html = '<div class="projects-single animate__animated animate__fadeIn" id="' + element.id + "-" + element.type + '" onclick="loadDLPage(this.id)">' +
             '<span class="projects-single-title">' + element.name + '</title></span><br>' +
             '<span class="projects-single-type">' + element.type + '</title></span><br>' +
             '<span class="projects-single-summary">' + element.summary + '</title></span>' +
